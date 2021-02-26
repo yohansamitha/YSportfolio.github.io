e9ba9df31729a4ddb678d3e0f3434a3f26e43cc8
YohanSamitha <yohansamitha123@gmail.com>
2021-02-27 05:04:42 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $(window).scroll(function () {
         if (this.scrollY > 20) {
+            $('.nav_item').addClass("sticky_color");
             $('.nav_bar').addClass("sticky");
         } else {
             $('.nav_bar').removeClass("sticky");
+            $('.nav_item').removeClass("sticky_color");
         }
     });
 });
